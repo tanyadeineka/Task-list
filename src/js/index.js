e8bad3c0b81189939baa09ee630a0ce1e88337fa
Tetiana Deineka <108138671+tanyadeineka@users.noreply.github.com>
2024-02-01 21:52:59 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,10 @@ window.onload = () => {
 const initialUISetup = () => {
     document.getElementById('search').addEventListener('mouseover', () => addClassWithTimeout('searchLabel', 'hovered', 500));
     document.getElementById('search').addEventListener('mouseout', () => removeClassWithTimeout('searchLabel', 'hovered', 500));
+    document.getElementById('search').addEventListener('keyup', (event) => searchInputHandler(event))
     document.getElementById('addTaskInput').addEventListener('keyup', (event) => validateField(event));
+    document.getElementById('addButton').addEventListener('click', addNewTask);
+    radioHandlersSetup();
 }
 
 const addClassWithTimeout = (id, className, timeout) => {
@@ -54,10 +57,22 @@ const enableAddTaskButton = () => {
 const addNewTask = () => {
     const inputElement = document.getElementById('addTaskInput');
     const taskWrapper = document.createElement('div');
+    const id = generateID(10);
     taskWrapper.innerHTML = getTaskTemplate(inputElement.value, id).trim();
+    document.getElementById('taskList').append(taskWrapper.firstChild);
+    inputElement.value = '';
+    disableAddTaskButton();
+    document.getElementById(id).addEventListener('click', (event) => onRadioClick(event));
 }
-const getTaskTemplate = (value) => {
-    return `<div class="task plainTask">
+
+const changeTaskStatus = (id) => {
+    const taskElement = document.getElementById(id);
+    taskElement.remove();
+    document.getElementById('doneTaskList').append(taskElement);
+}
+
+const getTaskTemplate = (value, id) => {
+    return `<div class="task plainTask" id="${id}">
                 <div class="taskName">
                     <div class="radio leftElement">
                         <img width="15" src="src/images/check.png">
@@ -65,4 +80,109 @@ const getTaskTemplate = (value) => {
                     <span class="taskTitle">${value}</span>
                 </div>
             </div>`
+}
+
+const generateID = (idLength) => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < idLength; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+const onRadioClick = (event) => {
+    const taskElement = event.target.closest('.task.plainTask');
+    if(taskElement){
+        changeTaskStatus(taskElement.getAttribute('id'));
+    }
+}
+
+const radioHandlersSetup = () => {
+    const radioButtons = document.getElementsByClassName('radio');
+    for (const radio of radioButtons) {
+        radio.removeEventListener('click', (event) => onRadioClick(event));
+        radio.addEventListener('click', (event) => onRadioClick(event));
+    }
+}
+
+const searchInputHandler = (event) => {
+    if (event.target.value.length > 0) {
+        filterTasks(event.target.value);
+    } else {
+        clearFiltration();
+    }
+}
+
+const filterTasks = (text) => {
+    const taskElementsList = document.getElementsByClassName('task');
+    for (const taskElement of taskElementsList){
+        const taskTitle = taskElement.getElementsByClassName('taskTitle')[0];
+        if(!taskTitle || !taskTitle.innerHTML.includes(text)) {
+            taskElement.classList.add('hidden');
+        }
+    }
+}
+
+const clearFiltration = () => {
+    const taskElementsList = document.getElementsByClassName('task');
+    for (const taskElement of taskElementsList) {
+        taskElement.classList.remove('hidden');
+    }
+}
+
+const stringConstants = {
+    localStorageKey: 'taskList',
+    localStorageDoneKey: 'doneTasks',
+}
+
+const storageTemplate = JSON.stringify({taskList: []});
+
+const initialStorageSetup = () => {
+    const initialStorage = getStorage(stringConstants.localStorageKey);
+    if (!initialStorage) {
+        localStorage.setItem(stringConstants.localStorageKey, storageTemplate);
+        localStorage.setItem(stringConstants.localStorageDoneKey, storageTemplate);
+    }
+}
+
+const getTaskStorageLocation = (id) => {
+    const activeStorage = getStorage(stringConstants.localStorageKey);
+    if (JSON.parse(activeStorage).taskList.find((task) => task.id.localeCompare(id) === 0)) {
+        return stringConstants.localStorageKey;
+    } else {
+        return stringConstants.localStorageDoneKey;
+    }
+}
+
+const getTasksFromStorage = (key) => {
+    const taskListString = getStorage(key);
+    if (!taskListString) {
+        initialStorageSetup();
+    }
+    return JSON.parse(getStorage(key)).taskList;
+}
+
+const addTaskToStorage = (task, key) => {
+    const taskListString = getStorage(key);
+    const taskList = JSON.parse(taskListString);
+    taskList.taskList.push(task);
+    localStorage.setItem(key, JSON.stringify(taskList));
+}
+
+const removeTaskFromStorage = (id, key) => {
+    const taskListString = getStorage(key);
+    const taskList = JSON.parse(taskListString);
+    const newList = taskList.taskList.filter((task) => {
+        if (task.id.localeCompare(id) === 0) {
+            addTaskToStorage(task, key.localeCompare(stringConstants.localStorageKey) === 0 ? stringConstants.localStorageDoneKey : stringConstants.localStorageKey);
+        } else {
+            return task;
+        }
+    });
+    localStorage.setItem(key, JSON.stringify({taskList: newList}));
+}
+
+const getStorage = (key) => {
+    return localStorage.getItem(key);
 }
